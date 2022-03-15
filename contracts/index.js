35f2d9169f87363893ac0b7fb5e50603f49ef362
sam bacha <sam@manifoldfinance.com>
2022-03-15 13:33:35 -0700

    contracts dir

---
+++ contracts/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const weirdERC20 = [
+"Approval.sol",
+"ApprovalToZero.sol",
+"BlockList.sol",
+"Bytes32Metadata.sol",
+"ERC20.sol",
+"HighDecimals.sol",
+"LowDecimals.sol",
+"MissingReturns.sol",
+"NoRevert.sol",
+"Pausable.sol",
+"Proxied.sol",
+"Reentrant.sol",
+"ReturnsFalse.sol",
+"RevertToZero.sol",
+"RevertZero.sol",
+"TransferFee.sol",
+"TransferFromSelf.sol",
+"Uint96.sol",
+"Upgradable.sol"
+];
+
+module.exports = weirdERC20;
